4aeac2d156b0ba654eb24fa751568495ea038f3b
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-10-21 11:13:36 +0200

    incorpora entidad contract

---
+++ app/src/main/java/es/upm/miw/SolitarioCelta/models/ResultadoContract.java
@@ -0,0 +1,19 @@
+package es.upm.miw.SolitarioCelta.models;
+
+import android.provider.BaseColumns;
+
+public class ResultadoContract {
+
+
+    private ResultadoContract() {
+    }
+
+    public static abstract class tablaResultado implements BaseColumns {
+        static final String TABLE_NAME = "resultados";
+
+        static final String COL_NAME_ID = _ID;
+        static final String COL_NAME_NOMBRE = "nombre";
+        static final String COL_NAME_FECHA = "fecha";
+        static final String COL_NAME_PIEZAS = "piezas";
+    }
+}
